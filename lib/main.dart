1a557ded7d01bd38cda55424ace30c95a3153096
Karim Ben Amara <karimbenamara91@gmail.com>
2024-08-29 18:19:21 +0100

    added app theme handling

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:walletapp/app_state/theme_provider.dart';
 import 'package:walletapp/firebase_options.dart';
 import 'package:walletapp/screens/home.dart';
 import 'package:walletapp/screens/lock_screen.dart';
@@ -47,32 +49,45 @@ void main() async {
     ],
   );
   final pinFuture = getUserPin();
+  final themeProvider = ThemeProvider();
+
   final config = Config();
 
   await config.init();
   await initNotificationsFuture;
   await initFirebaseFuture;
+  await themeProvider.init();
+
   final pin = await pinFuture;
 
   stopwatch.stop();
   debugPrint("Initialization took ${stopwatch.elapsedMilliseconds}ms");
-  runApp(MyApp(
-    pin: pin,
-    config: config,
-  ));
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => themeProvider,
+      builder: (context, child) => MyApp(
+        pin: pin,
+        config: config,
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
   final String? pin;
   final Config config;
-  const MyApp({super.key, this.pin, required this.config});
+
+  const MyApp({
+    super.key,
+    this.pin,
+    required this.config,
+  });
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
     super.initState();
@@ -80,19 +95,19 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Wallet',
-      darkTheme: ThemeData.dark(),
-      theme: ThemeData.from(
-          colorScheme: ColorScheme.fromSeed(
-        seedColor: Color.fromRGBO(9, 29, 51, 1.0),
-        dynamicSchemeVariant: DynamicSchemeVariant.vibrant,
-      )),
-      themeMode: ThemeMode.light,
-      home: ChangeNotifierProvider(
-        builder: (context, child) => screen(context, widget.config),
-        create: (BuildContext context) => widget.config,
-      )
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        debugPrint("themeMode ${themeProvider.themeMode}");
+        return MaterialApp(
+            title: 'Wallet',
+            darkTheme: themeProvider.darkTheme,
+            theme: themeProvider.themeData,
+            themeMode: themeProvider.themeMode,
+            home: ChangeNotifierProvider(
+              builder: (context, child) => screen(context, widget.config),
+              create: (BuildContext context) => widget.config,
+            ));
+      },
     );
   }
 
